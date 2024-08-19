ddd9eabf75a0f253d16771fe4d5501395561e858
Matheus AG <121025947+MatheusAGx@users.noreply.github.com>
2024-08-19 20:20:44 -0300

    menu criado para logout e visualização de dados

--- config/header/header.php
+++ config/header/header.php
@@ -258,13 +258,6 @@ if (!isset($_SESSION))
             </li>
           </ul>
         </li> -->
-
-      <li class="sidebar-footer">
-        <a href="/php/login" class="sidebar-link">
-        <i class="bi bi-box-arrow-left"></i>
-          <span>Logout</span>
-        </a>
-      </li>
     </aside>
 
     <div class="main p-3">
@@ -278,13 +271,13 @@ if (!isset($_SESSION))
               </li>
             </ul>
             <span class="navbar-text">
-              <button class="btn btn-light" id="logout">
+              <button class="btn btn-light" id="notificacao">
                 <i class="bi bi-bell"></i>
               </button>
               <?= $_SESSION['nome'] ?>
-              <button class="btn btn-dark" id="logout">
-                <i class="bi bi-box-arrow-left"></i>
-              </button>
+              <button class="btn btn-light" id="logout">
+                <i class="bi bi-person-circle"></i>
+              </button> 
             </span>
           </div>
         </div>
@@ -294,10 +287,5 @@ if (!isset($_SESSION))
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
       <script>
-      function logout() {
-        <?php 
-          session_destroy();
-          header('Location: ../login/index.php');
-        ?>
-      }
+      
       </script>
