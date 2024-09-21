6464548b7192035ee21e6cb30b5c13de60113d72
Matheus AG <121025947+MatheusAGx@users.noreply.github.com>
2024-09-21 19:01:35 -0300

    fix: index

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+include "../config/config.php";
+
+if (isset($_POST["acessar"])) {
+    $username = $_POST["username"];
+    $senhainicial = $_POST["senha"];
+
+    $senha = sha1($senhainicial);
+    
+
+    $q_busca_login = "SELECT * FROM usuarios WHERE (cpf = '$username' OR cnpj = '$username') AND senha = '$senha'";
+    $busca_login = $conn->query($q_busca_login);
+
+    $quantidade = $busca_login->num_rows;
+
+   
+    if ($quantidade > 0) {
+        $usuario = $busca_login->fetch_assoc();
+        session_start();
+
+        $_SESSION['id'] = $usuario['id'];
+        $_SESSION['nome'] = $usuario['nome'];
+
+        header("Location: ../dashboard");
+    } else {
+        echo "Falha ao logar! Email ou senha incorretos!";
+    }
+}
+
+include "view.php";
