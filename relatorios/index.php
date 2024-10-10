a62598e08e318943b55c2ba0b6899e70a51989a6
Matheus AG <121025947+MatheusAGx@users.noreply.github.com>
2024-10-09 21:59:49 -0300

    feat: gráficos e estatístiscas no relatorio

--- relatorios/index.php
+++ relatorios/index.php
@@ -1,7 +1,58 @@
 <?php
 include "../config/config.php";
-$q_busca_chave = "SELECT *,(SELECT descricao FROM chave_instituicao WHERE id = id_instituicao) as instituicao, (SELECT descricao FROM chave_status WHERE id = id_status) as status, (SELECT nome FROM usuarios WHERE id = id_usuario) as usuario FROM chave";
+$q_busca_chave = "SELECT *,(SELECT descricao FROM chave_instituicao WHERE id = id_instituicao) as instituicao, (SELECT descricao FROM chave_status WHERE id = id_status) as status, (SELECT nome FROM usuarios WHERE id = id_usuario) as usuario FROM chave LIMIT 12";
 //$q_busca_chave = "SELECT nome, id_instituicao, id_status, id_usuario, (SELECT descricao FROM chave_instituicao) as instituicao, (SELECT descricao FROM chave) as status, (SELECT nome FROM usuarios) as usuarios FROM chave";
 $busca_chave = $conn->query($q_busca_chave);
+
+//CONTA CHAVES
+$q_total_chave = "SELECT COUNT(*) FROM chave";
+$total_chave = $conn->query($q_total_chave);
+$t_chave = $total_chave->fetch_array();
+
+$q_chave_disp = "SELECT COUNT(*) FROM chave WHERE id_status = 1";
+$chave_disp = $conn->query($q_chave_disp);
+$d_chave = $chave_disp->fetch_array();
+
+$q_chave_ocup = "SELECT COUNT(*) FROM chave WHERE id_status = 2";
+$chave_ocup = $conn->query($q_chave_ocup);
+$oc_chave = $chave_ocup->fetch_array();
+
+//CONTA REGISTROS POR MÊS
+$q_registro_agosto = "SELECT COUNT(id) FROM registros WHERE data_registro BETWEEN '2024-08-01' AND '2024-09-01'";
+$registro_agosto = $conn->query($q_registro_agosto);
+$reg_agosto = $registro_agosto->fetch_array();
+
+$q_registro_setembro = "SELECT COUNT(id) FROM registros WHERE data_registro BETWEEN '2024-09-01' AND '2024-10-01'";
+$registro_setembro = $conn->query($q_registro_setembro);
+$reg_setembro = $registro_setembro->fetch_array();
+
+$q_registro_outubro = "SELECT COUNT(id) FROM registros WHERE data_registro BETWEEN '2024-10-01' AND '2024-11-01'";
+$registro_outubro = $conn->query($q_registro_outubro);
+$reg_outubro = $registro_outubro->fetch_array();
+
+$q_registro_novembro = "SELECT COUNT(id) FROM registros WHERE data_registro BETWEEN '2024-11-01' AND '2024-12-01'";
+$registro_novembro = $conn->query($q_registro_novembro);
+$reg_novembro = $registro_novembro->fetch_array();
+
+$q_registro_dezembro = "SELECT COUNT(id) FROM registros WHERE data_registro BETWEEN '2024-12-01' AND '2025-01-01'";
+$registro_dezembro = $conn->query($q_registro_dezembro);
+$reg_dezembro = $registro_dezembro->fetch_array();
+
+//CONTA REGISTRO POR INSTITUICAO
+$q_registro_ft = "SELECT COUNT(id) FROM registros WHERE id_instituicao = 1";
+$registro_ft = $conn->query($q_registro_ft);
+$reg_ft = $registro_ft->fetch_array();
+
+$q_registro_fca = "SELECT COUNT(id) FROM registros WHERE id_instituicao = 2";
+$registro_fca = $conn->query($q_registro_fca);
+$reg_fca = $registro_fca->fetch_array();
+
+$q_registro_cotil = "SELECT COUNT(id) FROM registros WHERE id_instituicao = 3";
+$registro_cotil = $conn->query($q_registro_cotil);
+$reg_cotil = $registro_cotil->fetch_array();
+
+$q_registro_portaria = "SELECT COUNT(id) FROM registros WHERE id_instituicao = 4";
+$registro_portaria = $conn->query($q_registro_portaria);
+$reg_portaria = $registro_portaria->fetch_array();
 
 include "view.php";
--- relatorios/view.php
+++ relatorios/view.php
@@ -12,44 +12,12 @@
         </div>  
     </div>
 
-    <!-- <div class="card my-2">
-        <div class="card-body">
-            <h5>Filtros</h5>
-            <form action="" method="post" id="formFiltro">
-                <div class="row">
-                    <div class="col-4">
-                        <small><strong>Chave:</strong></small>
-                        <input class="form-control" type="text" name="filtro_chave" id="filtro_chave" placeholder="Digite o nome da chave">
-                    </div>
-                    <div class="col-4">
-                        <small><strong>Usuario:</strong></small>
-                        <input class="form-control" type="text" name="filtro_usuario" id="filtro_usuario" placeholder="Digite o nome do usuário">
-                    </div>
-                    <div class="col-4">
-                        <small><strong>Data de Devolução:</strong></small>
-                        <input class="form-control" type="date" name="filtro_data_dev" id="filtro_data_dev">
-                    </div>
-                </div>
-                <div class="row">
-                <div class="col-6 mt-2">
-                    <button class="btn btn-primary" type="submit" name="filtrar" id="filtrar">Filtrar</button>
-                </div>
-                <div class="col-6 mt-2">
-                    <?php if(isset($_POST['filtrar'])) { ?>
-                    <a class="btn btn-danger" href="./index.php" style="float:right;">Limpar Filtro</a>
-                    <?php } ?>
-                </div>
-                </div>
-            </form>
-        </div>
-    </div> -->
-
     <div class="card my-2">
         <div class="card-body">
             <div class="row">
             <h4 class="mb-3"><b>Status Geral</b></h4>
-            <small><button style="background-color: rgb(96, 235, 177); width: 15px; height: 15px; border-radius: 50%;"></button> Chave Disponível</small>
-            <small><button class="mb-3" style="background-color: rgb(245, 103, 103); width: 15px; height: 15px; border-radius: 50%;"></button> Chave Ocupada</small>
+            <small><button style="background-color: rgb(96, 235, 177); width: 15px; height: 15px; border-radius: 50%;"></button> Chave Disponível - <?=$d_chave['COUNT(*)']?></small>
+            <small><button class="mb-3" style="background-color: rgb(245, 103, 103); width: 15px; height: 15px; border-radius: 50%;"></button> Chave Ocupada - <?=$oc_chave['COUNT(*)']?></small>
             <?php while ($chave = $busca_chave->fetch_object()) { ?>
                 <div class="col-4 my-2 text-center">
                     <div class="card">
@@ -63,8 +31,72 @@
                 </div>
                 <?php } ?>
             </div>
+            <h6>Total de chaves: <?=$t_chave['COUNT(*)']?></h6>
         </div>  
     </div>
+
+    <div class="card my-2">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-6">
+                    <h4 class="p-1" style="text-align:center;"><b>Registros por Mês</b></h4>
+                    <canvas id="reg_mês"></canvas>
+                </div>
+                <div class="col-6">
+                    <h4 class="p-1" style="text-align:center;"><b>Registros por Instituição</b></h4>
+                    <canvas id="reg_instituicao"></canvas>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="card my-2">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-6">
+                    <h4 class="p-1" style="text-align:center;"><b>Registros por Mês</b></h4>
+                    <canvas id="ocorrencia_mês"></canvas>
+                </div>
+                <div class="col-6">
+                    <h4 class="p-1" style="text-align:center;"><b>Registros por Instituição</b></h4>
+                    <canvas id="ocorrencia_instituicao"></canvas>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script>
+
+        const ctx = document.getElementById('reg_mês');
+        new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ['Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+            datasets: [{
+            label: 'Registros',
+            data: [<?=$reg_agosto['COUNT(id)']?>, <?=$reg_setembro['COUNT(id)']?>, <?=$reg_outubro['COUNT(id)']?>, <?=$reg_novembro['COUNT(id)']?>, <?=$reg_dezembro['COUNT(id)']?>],
+            fill: false,
+            borderColor: 'rgb(75, 192, 192)',
+            tension: 0.1
+        }]
+        },
+        });
+
+        const ctz = document.getElementById('reg_instituicao');
+
+        new Chart(ctz, {
+        type: 'bar',
+        data: {
+        labels: ['Unicamp - FT', 'Unicamp - FCA', 'Cotil', 'Portaria'],
+        datasets: [{
+            label: 'Registros',
+            data: [<?=$reg_ft['COUNT(id)']?>, <?=$reg_fca['COUNT(id)']?>, <?=$reg_cotil['COUNT(id)']?>, <?=$reg_portaria['COUNT(id)']?>],
+            borderWidth: 1
+            }]
+        },
+        });
+
+    </script>
 
 <?php 
     include "../config/footer/footer.php";
